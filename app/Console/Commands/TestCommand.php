465c6b74ee4df43b41fad554d1a975c7767077e1
dsoloview <dsoloview44@gmail.com>
2024-04-21 21:35:37 +0200

    horizon installed

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -2,18 +2,16 @@
 
 namespace App\Console\Commands;
 
-use Database\Seeders\Icon\IconSeeder;
 use Illuminate\Console\Command;
 
 class TestCommand extends Command
 {
-    protected $signature = 'test';
+    protected $signature = 'test:test';
 
     protected $description = 'Command description';
 
     public function handle(): void
     {
-        $seeder = new IconSeeder();
-        $seeder->run();
+        $this->info('Test command');
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('fetch:exchange-rates')->daily();
+        $schedule->command('test:test')->everyMinute();
     }
 
     /**
